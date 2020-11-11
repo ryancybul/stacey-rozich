60c28499714fefc15091220dcdb1615f88c24f03
Ryan Cybul <ryan.cybul@gmail.com>
2020-11-11 18:43:48 -0500

    Lightbox test Blog

--- src/components/gallery/Artwork-copy.js
+++ src/components/gallery/Artwork-copy.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
 import Img from 'gatsby-image';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { SRLWrapper } from 'simple-react-lightbox';
+import { studioArtQuery } from '../../queries/studioArtQuery';
 
-const ArtworkCopy = ({ artwork }) => {
+const ArtworkCopy = () => {
+  const data = studioArtQuery().allWordpressWpMedia.edges;
+  const [artwork, setArtwork] = useState([]);
+
+  useEffect(() => {
+    setArtwork(
+      data.map(function(image) {
+        return {
+          alt: image.node.alt_text,
+          title: image.node.title,
+          fluid: image.node.localFile.childImageSharp.fluid,
+          id: image.node.id,
+          srcSet: image.node.localFile.childImageSharp.fluid.srcSet,
+          sizes: image.node.localFile.childImageSharp.fluid.sizes,
+          width: image.node.localFile.childImageSharp.original.width,
+          height: image.node.localFile.childImageSharp.original.height,
+          key: image.node.localFile.childImageSharp.fluid.src,
+        };
+      })
+    );
+  }, [data]);
+
   const options = {
     settings: { disablePanzoom: true },
     caption: { showCaption: false },
@@ -48,22 +70,5 @@ const GalleryWrapper = styled.div`
   @media only screen and (max-width: 650px) {
     padding: 0 0.25rem;
     margin-top: 100px;
-  }
-`;
-
-const ImageWrapper = styled.div`
-  position: relative;
-  padding: 10px;
-  text-align: center;
-  :focus {
-    outline: none;
-  }
-  iframe {
-    top: 0;
-    left: 0;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    padding: 10px;
   }
 `;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,18 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import SEO from '../components/seo';
 import Smiley from '../components/Smiley';
+import ArtworkCopy from '../components/gallery/Artwork-copy';
 
 const blog = () => (
   <Wrapper>
     <SEO title="Blog" />
-    <h1>¯\_(ツ)_/¯</h1>
+    <ArtworkCopy />
   </Wrapper>
 );
 export default blog;
 
 const Wrapper = styled.div`
-  align-items: center;
+  /* align-items: center;
   display: flex;
   height: 80vh;
-  justify-content: center;
+  justify-content: center; */
 `;
