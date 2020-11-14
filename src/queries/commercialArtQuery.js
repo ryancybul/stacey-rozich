b6cbfc62db849b3e0326ee9b1b8a6b677c320015
Ryan Cybul <ryan.cybul@gmail.com>
2020-11-14 16:49:35 -0500

    Image optimization

--- src/queries/commercialArtQuery.js
+++ src/queries/commercialArtQuery.js
@@ -33,8 +33,8 @@ export const commercialArtQuery = () => {
                 }
                 fluid(
                   quality: 75
-                  srcSetBreakpoints: [400, 600, 700, 800]
-                  sizes: "(max-width: 415px) 400px,(max-width: 1200px) 600px, (max-width: 1800px) 700px, 100vw"
+                  srcSetBreakpoints: [400, 500, 600, 700]
+                  sizes: "(max-width: 650px) calc(98.5vw - 20px),(max-width: 1200px) calc(49.25vw - 40px), (max-width: 1920px) calc(32.83vw - 60px), 100vw"
                 ) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
